3b35e52c5e829af6eabdcd29bb4bc37e7742f86d
linoj ravindran <linojravindran@linojs-mbp>
2016-02-21 16:59:35 +0100

    Initial Commit

---
+++ Check ListeTests/Check_ListeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Check_ListeTests.swift
+//  Check ListeTests
+//
+//  Created by linoj ravindran on 21/02/2016.
+//  Copyright © 2016 linoj ravindran. All rights reserved.
+//
+
+import XCTest
+@testable import Check_Liste
+
+class Check_ListeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
